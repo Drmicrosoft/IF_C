a4d810c455225469f614e6c25a623b1a81eb274c
Omar Okasha <omar.prmail@gmail.com>
2016-12-15 15:00:28 +0200

    Create if.cpp
    if ( x == 0 ) 
    if ( x != 0 ) 
    if ( x > 0 ) 
    if ( x < 0 ) 
    if ( x >= 0 ) 
    if ( x <= 0 ) 
    if ( x >= 0 && y <=0 ) 
    if ( x >= 0 || y <=0 )

---
+++ if.cpp
@@ -0,0 +1,29 @@
+#include	<stdio.h>
+
+int main ()
+{
+	int x = 5 ;
+	int y = 5 ;
+	int z = 5 ;
+	if (x != 5 ) 
+	{if(y==6)
+		{
+	if(z>2)
+	{
+	printf("hello");
+}
+else 
+printf("omar");
+}
+else 
+printf("kkadkasdkasdkasdk");
+}
+
+
+
+if(x==5 && y !=6 && z >2)
+printf("\ndone");
+if(x!=5 || y !=6 || z <2)
+printf("\ndone");
+}
+
